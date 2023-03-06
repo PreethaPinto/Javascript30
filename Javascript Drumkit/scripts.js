bb1a505f7f68171398f74d23119db1e45c6efa73
PreethaPinto <preethacp@gmail.com>
2023-03-06 16:27:51 +1000

    New project folder CSS + JS clock added

--- Javascript Drumkit/scripts.js
+++ Javascript Drumkit/scripts.js
@@ -3,15 +3,17 @@ window.addEventListener("keydown", function (e) {
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if (!audio) return;
 
-  audio.currentTime = 0;
+  audio.currentTime = 0; //rewind to start
   audio.play();
   key.classList.add("playing");
 });
 
 function removeTransition(e) {
+  // console.log(e);
   if (e.propertyName !== "transform") return;
-  this.classList.remove(".playing");
+  this.classList.remove("playing");
 }
 
 const keys = document.querySelectorAll(".key");
 keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound);
